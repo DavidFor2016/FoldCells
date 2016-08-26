d8dc9babbd409d9b935153f92612b3308263c122
Yesi <317447721@qq.com>
2016-08-26 10:05:54 +0800

    Initial Commit

---
+++ FoldCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoldCells
+//
+//  Created by Yesi on 16/8/26.
+//  Copyright © 2016年 Yesi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
